56bd6ffdc40f61c00a632489fa881b2576809112
Brandon Leech <bleech94@hotmail.com>
2016-07-06 16:15:43 -0700

    first commit

---
+++ www/js/my-app.js
@@ -0,0 +1,96 @@
+// Initialize app
+var myApp = new Framework7();
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+var tempThreshold = 30;
+var hours = 1;
+var minutes = '00'
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+
+});
+
+// Using page callback for page:
+myApp.onPageInit('settings', function (page) {
+  // Set threshold value initially
+  $$('#temperatureThreshold').text(tempThreshold.toString() + '° C');
+
+  // Add threshold inc/dec handlers
+  $$('#incTempThreshold').on('click', function(){
+    tempThreshold++;
+    $$('#temperatureThreshold').text(tempThreshold.toString() + '° C');
+  });
+  $$('#decTempThreshold').on('click', function(){
+    tempThreshold--;
+    $$('#temperatureThreshold').text(tempThreshold.toString() + '° C');
+  });
+
+  var today = new Date();
+
+  var pickerInline = myApp.picker({
+      input: '#movementThreshold',
+      container: '#picker-date-container',
+      toolbar: false,
+      rotateEffect: true,
+
+      value: [hours, minutes],
+
+      formatValue: function (p, values) {
+          return values[0] + ' hours ' + values[1] + ' minutes';
+      },
+
+      cols: [
+          // Hours
+          {
+              values: (function () {
+                  var arr = [];
+                  for (var i = 0; i <= 12; i++) { arr.push(i); }
+                  return arr;
+              })(),
+          },
+          // Divider
+          {
+              divider: true,
+              content: ':'
+          },
+          // Minutes
+          {
+              values: (function () {
+                  var arr = [];
+                  for (var i = 0; i <= 59; i++) { arr.push(i < 10 ? '0' + i : i); }
+                  return arr;
+              })(),
+          }
+      ]
+  });
+})
+
+// TODO Ajax GET to update temp + time values
+function refresh() {
+  alert('refresh()');
+  //$('$currentTemperature').html('69');
+}
+
+// TODO Ajax GET/POST to enable/disable the stove. May want to have 2 functions.
+function toggleStove() {
+  alert('toggleStove()');
+}
+
+function adjustTempThreshold(adjustment) {
+  alert('adjustTempThreshold(' + adjustment + ')');
+  tempThreshold += adjustment;
+}
+
+// TODO Ajax POST to change thresholds
+function applySettings() {
+  alert('applySettings()');
+}
